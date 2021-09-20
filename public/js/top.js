7ebd67409a309d0ad1450b772bebbabcc67c9a56
kinouchi1000 <drums.radwimps203@gmail.com>
2021-09-20 20:08:15 +0900

    fix: fixed check of phone number bugs

---
+++ public/js/top.js
@@ -0,0 +1,9 @@
+$(function() {
+  history.pushState(null, null, null); //ブラウザバック無効化
+  //ブラウザバックボタン押下時
+  $(window).on("popstate", function (event) {
+    history.pushState(null, null, null);
+    window.alert('前のページには戻れません。');
+  });
+}); 
+
